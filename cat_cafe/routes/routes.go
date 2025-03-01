60773e02764ec4ef193a071c579e760da752d801
panpradap <163168413+panpradap001@users.noreply.github.com>
2025-03-01 22:19:45 +0700

    Update routes.go

--- cat_cafe/routes/routes.go
+++ cat_cafe/routes/routes.go
@@ -7,6 +7,8 @@ import (
 
 // API Path
 func SetupRoutes(router *gin.Engine, catController *controllers.CatController) {
-    router.POST("/cats", catController.CreateCat)
-    router.GET("/cats", catController.GetCats)
+	router.POST("/cats", catController.CreateCat)
+	router.GET("/cats", catController.GetCats)
+	router.PUT("/cats/:id", catController.UpdateCat)
+	router.DELETE("/cats/:id", catController.DeleteCat)
 }
